5f13f4abcaa2b137b6daf362a391fdc2a8da9795
zzaain <sheriellaghzain@gmail.com>
2026-04-23 15:47:10 +0700

    menambahkan search dan delete

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -44,6 +44,35 @@ class LinkedList
         nodeBaru->next =START;
         START = nodeBaru;
         return;
-     
     }
-};
+     
+     bool search (int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current ->noMhs)
+        {
+            previous = current;
+            current = current -> next;
+        }
+
+        return (current != NULL);
+    }
+
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim,previous,current))
+        return false;
+
+        if (current == START)
+        START = START->next;
+        else previous->next = current->next;
+        delete current;
+        return true;
+
+    }
+     };
