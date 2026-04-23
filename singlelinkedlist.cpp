ccc15fe7137141dcc368abb12a9e0a41fa0bd0eb
zzaain <sheriellaghzain@gmail.com>
2026-04-23 15:49:30 +0700

    menambahkan traverse

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -75,4 +75,22 @@ class LinkedList
         return true;
 
     }
-     };
+       void traverse (){
+    if (listEmpty())
+    {
+        cout << "\nList Kosong\n";
+    }
+    else
+    {
+        cout << "\nData di dalam list adalah\n";
+        Node *currentNode = START;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << endl;
+            currentNode = currentNode->next;
+        }
+        cout << endl;
+    }
+}
+};
