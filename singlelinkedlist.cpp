5a9d8e5880a68f82ece68cf4540fa72e4118a2df
zzaain <sheriellaghzain@gmail.com>
2026-04-23 15:42:02 +0700

    menambahkan logika insert

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -32,5 +32,18 @@ class LinkedList
 
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
-     }
+
+         if (START == NULL || nim <= START->noMhs)
+     {
+        if (START != NULL && nim == START->noMhs)
+        {
+            cout << "/nDuplikasi noMhs tidak diijinkan\n";
+            return;
+        }
+
+        nodeBaru->next =START;
+        START = nodeBaru;
+        return;
+     
+    }
 };
